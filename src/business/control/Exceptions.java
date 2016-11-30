3ff7a01aa6d1eb06293ef911c3a623b1c098e06c
Renê <renealves@lavid.ufpb.br>
2016-11-30 17:34:24 -0300

    create ultis string in exeptions

--- src/business/control/Exceptions.java
+++ src/business/control/Exceptions.java
@@ -12,27 +12,32 @@ import utils.StringUtils;
  * @author Ruan
  */
 public class Exceptions {
+    StringUtils strUltil;
+    
+    public Exceptions(){
+        strUltil = new StringUtils();
+    } 
     public void verificarAuth(boolean authStatus) throws InvalidAuthException{
         if(!authStatus){
-            throw new InvalidAuthException(StringUtils.ERRO_AUTH);
+            throw new InvalidAuthException(strUltil.ERRO_AUTH);
         }
     }
     
     public void verificarAuthCarona(boolean authStatus, ETipoUser authTipo) throws InvalidAuthException{
         if(authTipo != ETipoUser.carona  || authTipo == null){
-            throw new InvalidAuthException(StringUtils.ERRO_PRIVILEGIO_CARONA);
+            throw new InvalidAuthException(strUltil.ERRO_PRIVILEGIO_CARONA);
         }
     }
     
     public void verificarAuthMotorista(boolean authStatus, ETipoUser authTipo) throws InvalidAuthException{
         if(authTipo != ETipoUser.motorista  || authTipo == null){
-            throw new InvalidAuthException(StringUtils.ERRO_PRIVILEGIO_MOTORISTA);
+            throw new InvalidAuthException(strUltil.ERRO_PRIVILEGIO_MOTORISTA);
         }
     }
     
     public void verificarAuthAdministrador(boolean authStatus, ETipoUser authTipo) throws InvalidAuthException{
         if(authTipo != ETipoUser.administrador  || authTipo == null){
-            throw new InvalidAuthException(StringUtils.ERRO_PRIVILEGIO_ADMINISTRADOR);
+            throw new InvalidAuthException(strUltil.ERRO_PRIVILEGIO_ADMINISTRADOR);
         }
     }
 }
